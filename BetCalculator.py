b16fddd18eed4418a326d764d6f3addc2c719712
Fionn Bracken <fionnbracken@Fionns-MacBook-Air.local>
2020-11-23 23:09:49 +0000

    Added compute button

--- BetCalculator.py
+++ BetCalculator.py
@@ -23,24 +23,46 @@ class BetCalculator:
         master.title("Bet Calculator")
 
         Label(master, text = "Stake").grid(row=1, column = 1)
+        Label(master, text = "Odds").grid(row=2, column = 1)
         Label(master, text = "Each-Way").grid(row=1, column = 3)
-        Label(master, text = "Total Stake").grid(row=2, column=1)
-        Label(master, text = "Payout").grid(row=2, column=3)
+        Label(master, text = "E/W Terms").grid(row = 2, column = 3)
+        Label(master, text = "Total Stake").grid(row=3, column=1)
+        Label(master, text = "Payout").grid(row=3, column=3)
 
         self.stakeinput = StringVar()
         Entry(master, width=10, textvariable = self.stakeinput, justify=RIGHT).grid(row=1, column=2)
 
+        self.odds = StringVar()
+        Entry(master, width=10, textvariable = self.odds, justify = RIGHT).grid(row=2, column=2)
+
         self.ewinput = IntVar()
-        r1 = Radiobutton(master, variable=self.ewinput, justify=RIGHT).grid(row=1, column=4)
+        r1 = Radiobutton(master, variable=self.ewinput, value=True, justify=RIGHT).grid(row=1, column=4)
+        r2 = Radiobutton(master, variable=self.ewinput, value=False, justify=RIGHT).grid(row=1, column=5)
+
+        self.ewterms = StringVar()
+        Entry(master, width=10, textvariable = self.ewterms, justify=RIGHT).grid(row=2, column=4)
 
         self.totalstakes = StringVar()
-        lblTotalStakes = Label(master, textvariable=self.totalstakes).grid(row=2, column=2)
+        lblTotalStakes = Label(master, textvariable=self.totalstakes).grid(row=3, column=2)
 
         self.payout = StringVar()
-        lblPayout = Label(master, textvariable=self.payout).grid(row=2, column=4)
+        lblPayout = Label(master, textvariable=self.payout).grid(row=3, column=4)
 
-        self.totalstakes.set('100')
-        self.payout.set('1000')
+        #self.totalstakes.set('100')
+        #self.payout.set('1000')
+
+
+
+        btComputeStakes = Button(master, text = "Compute Payout", command=self.computePayout).grid(row=4, column = 4)
+
+
+    def computePayout(self):
+        
+        
+        bet = Accum(stakes = float(self.stakeinput.get()), odds=[float(self.odds.get())], ew=self.ewinput.get(), ewterms=[float(self.ewterms.get())])
+        self.totalstakes.set(bet.totalstakes())
+        self.payout.set(bet.payout())
+
 
 #Step 5: Execution
 if __name__=='__main__':
